a859a0834a90c636f1a92e7bc0ad867dde860da1
theopoipoi <161461@student.umons.ac.be>
2018-05-13 01:07:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/theopoipoi/exercice_integre/User.java
@@ -0,0 +1,23 @@
+package com.example.theopoipoi.exercice_integre;
+
+public class User {
+    private String username;
+    private int password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+}
